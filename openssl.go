96a3182096103db332b7d749f2eedda1970c3b04
richardpct <noreply>
2018-11-12 21:22:53 +0100

    first commit

---
+++ openssl.go
@@ -0,0 +1,97 @@
+// openssl package
+package main
+
+import (
+	"flag"
+	"fmt"
+	"github.com/richardpct/pkgsrc"
+	"log"
+	"os"
+	"os/exec"
+	"path"
+)
+
+var destdir = flag.String("destdir", "", "directory installation")
+var pkg pkgsrc.Pkg
+
+const (
+	name     = "openssl"
+	vers     = "1.0.2p"
+	ext      = "tar.gz"
+	url      = "https://www.openssl.org/source"
+	hashType = "sha256"
+	hash     = "50a98e07b1a89eb8f6a99477f262df71c6fa7bef77df4dc83025a2845c827d00"
+)
+
+func checkArgs() error {
+	if *destdir == "" {
+		return fmt.Errorf("Argument destdir is missing")
+	}
+	return nil
+}
+
+func configure() {
+	fmt.Println("Waiting while configuring ...")
+	cmd := exec.Command("/usr/bin/perl",
+		"./Configure",
+		"darwin64-x86_64-cc",
+		"--prefix="+*destdir,
+		"--openssldir="+*destdir+"/etc/openssl")
+	if out, err := cmd.Output(); err != nil {
+		log.Fatal(err)
+	} else {
+		fmt.Printf("%s\n", out)
+	}
+}
+
+func build() {
+	fmt.Println("Waiting while compiling ...")
+	cmd := exec.Command("make", "depend")
+	if out, err := cmd.Output(); err != nil {
+		log.Fatal(err)
+	} else {
+		fmt.Printf("%s\n", out)
+	}
+
+	cmd = exec.Command("make")
+	if out, err := cmd.Output(); err != nil {
+		log.Fatal(err)
+	} else {
+		fmt.Printf("%s\n", out)
+	}
+}
+
+func install() {
+	fmt.Println("Waiting while installing ...")
+	cmd := exec.Command("make", "install")
+	if out, err := cmd.Output(); err != nil {
+		log.Fatal(err)
+	} else {
+		fmt.Printf("%s\n", out)
+	}
+}
+
+func main() {
+	flag.Parse()
+	if err := checkArgs(); err != nil {
+		log.Fatal(err)
+	}
+
+	pkg.Init(name, vers, ext, url, hashType, hash)
+	pkg.CleanWorkdir()
+	if !pkg.CheckSum() {
+		pkg.DownloadPkg()
+	}
+	if !pkg.CheckSum() {
+		log.Fatal("Package is corrupted")
+	}
+
+	pkg.Unpack()
+	wdPkgName := path.Join(pkgsrc.Workdir, pkg.PkgName)
+	if err := os.Chdir(wdPkgName); err != nil {
+		log.Fatal(err)
+	}
+	configure()
+	build()
+	install()
+}
